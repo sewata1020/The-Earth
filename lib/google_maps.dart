bf3138e70236608a06a013c72957fd3845bd9cc4
Koichi Imamura <sewata1020@gmail.com>
2020-01-10 11:46:48 +0900

    first, error king

---
+++ lib/google_maps.dart
@@ -0,0 +1,76 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMaps extends StatefulWidget {
+  @override
+  _GoogleMapsState createState() => _GoogleMapsState();
+}
+
+class _GoogleMapsState extends State<GoogleMaps> {
+  LocationData currentLocation;
+  // StreamSubscription<LocationData> locationSubscription;
+
+  Location _locationService = new Location();
+  String error;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initPlatformState();
+    _locationService.onLocationChanged().listen((LocationData result) async {
+      setState(() {
+        currentLocation = result;
+      });
+    });
+  }
+
+  Completer<GoogleMapController> _controller = Completer();
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (currentLocation == null) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Flutter Maps"),
+          ),
+          body: GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition( // 最初のカメラ位置
+              target: LatLng(
+                  currentLocation.latitude, currentLocation.longitude),
+              zoom: 17.0,
+            ),
+          ),
+        ),
+      );
+    }
+  }
+  void initPlatformState() async {
+    LocationData myLocation;
+    try {
+      myLocation = await _locationService.getLocation();
+      error = "";
+    }on PlatformException catch(e){
+      if(e.code == 'PERMISSION_DENITED')
+        error = 'Permission denited';
+      else if(e.code == 'PERMISSION_DENITED_NEVER_ASK')
+        error = 'Permission denited - please ask the user to enable it from the app settings';
+      myLocation = null;
+    }
+    setState(() {
+      currentLocation = myLocation;
+    });
+  }
+}
